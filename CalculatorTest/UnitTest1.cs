8ac6b9a1652f1977d028aed5eaedc6ec38573c24
Luigidefilippo <defilippoluigi65@gmail.com>
2023-09-25 15:32:59 +0200

    Somma succes

--- CalculatorTest/UnitTest1.cs
+++ CalculatorTest/UnitTest1.cs
@@ -1,16 +1,27 @@
+using net_calculator_tester;
+
 namespace CalculatorTest
 {
     public class Tests
     {
+        Calculator calculator;
+
         [SetUp]
         public void Setup()
         {
+            calculator = new Calculator();
         }
 
         [Test]
-        public void Test1()
+        public void TestAddFloat()
         {
-            Assert.Pass();
+            float x = 3.6f;
+            float y = 1.5f;
+
+            float realOutput = 5.1f;
+            float sum = calculator.Add(x, y);
+
+            Assert.IsTrue(realOutput == sum , $"Il risultato corretto è {realOutput}, il tuo risulato è {sum}");
         }
     }
 }
